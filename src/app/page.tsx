76a0649d3558c122f9404d6dd85eb8353e0cbec2
jonas lima <jonaslimahpessoal@gmail.com>
2024-05-03 14:18:09 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { Boddy } from "./Boddy";
-import { Header } from "./Header";
-import { Image } from "./Image";
+import { Boddy } from "./components/Boddy";
+import { Header } from "./components/Header";
+import { Image } from "./components/Image";
 
 const page = () => {
   return(
